a85beda317113328c6fc9b2697e0ab026f56e9b5
Michael Stapelberg <stapelberg@debian.org>
2013-06-30 14:27:46 +0200

    initial commit

---
+++ lib/Debian/Debhelper/Sequence/golang.pm
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Debian::Debhelper::Dh_Lib;
+
+#insert_before("dh_installinit", "dh_systemd_enable");
+
+#insert_after("dh_installinit", "dh_systemd_start");
+
+1
