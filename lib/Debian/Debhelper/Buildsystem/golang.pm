14e556ec177628c1a801aa93ee3f67045cd768f2
Michael Stapelberg <stapelberg@debian.org>
2013-07-30 11:26:14 +0200

    Install files into /usr/share/golang/src and /usr/bin by default (Thanks pkern)
    This might break existing .install files, sorry about that. The new
    paths are a better default, though, for simple packages.

--- lib/Debian/Debhelper/Buildsystem/golang.pm
+++ lib/Debian/Debhelper/Buildsystem/golang.pm
@@ -120,11 +120,11 @@ sub install {
     my $this = shift;
     my $destdir = shift;
 
-    $this->doit_in_builddir('mkdir', '-p', $destdir);
-    $this->doit_in_builddir('cp', '-r', 'bin', $destdir);
+    $this->doit_in_builddir('mkdir', '-p', "$destdir/usr");
+    $this->doit_in_builddir('cp', '-r', 'bin', "$destdir/usr");
 
     # Path to the src/ directory within $destdir
-    my $dest_src = "$destdir/src/$ENV{DH_GOPKG}";
+    my $dest_src = "$destdir/usr/share/golang/src/$ENV{DH_GOPKG}";
     $this->doit_in_builddir('mkdir', '-p', $dest_src);
     $this->doit_in_builddir('cp', '-r', '-T', "src/$ENV{DH_GOPKG}", $dest_src);
 }
