c304c539f3a8df70725c623db1c94d4b0d7755a8
Michael Hudson-Doyle <michael.hudson@canonical.com>
2014-11-27 23:50:12 +0000

    make shlibs:Depends refer to precise ABI for a go shared library

--- lib/Debian/Debhelper/Sequence/golang.pm
+++ lib/Debian/Debhelper/Sequence/golang.pm
@@ -4,6 +4,8 @@ use strict;
 use Debian::Debhelper::Dh_Lib;
 
 insert_before('dh_gencontrol', 'dh_golang');
+insert_before('dh_makeshlibs', 'dh_makegolangshlibs');
+remove_command('dh_makeshlibs');
 
 # XXX: -u is deprecated, but we cannot use “-- -Zxz” because additional command
 # options will be appended (“-O--buildsystem=golang”), resulting in
